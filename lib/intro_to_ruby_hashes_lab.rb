b99f01680fd8453bf4ebc6f416d5caf9fae01e0a
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-25 14:54:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,10 @@ my_hash = {door: 1}
 end
 
 def pioneer
-pioneer = {:name => 'Grace Hopper'}  
+pioneer = {:name => 'Grace Hopper'}
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+{:id => number}
 end
